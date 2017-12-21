a811b593dab9796a37bcd53e732478215dc4905d
ccbikai(👎🏻🍜) <ccbikai@qq.com>
2017-12-21 17:39:33 +0800

    img lazyload

--- docs/src/js/index.js
+++ docs/src/js/index.js
@@ -8,4 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(link)
     link.setAttribute('target', '_blank')
   })
+
+  const lazyload = Array.from(document.querySelectorAll('[data-src]'))
+
+  lazyload.forEach(item => {
+    item.setAttribute('src', item.getAttribute('data-src'))
+  })
+}, false)
+
+window.addEventListener('load', () => {
+  const lazyload = Array.from(document.querySelectorAll('[data-src]'))
+
+  lazyload.forEach(item => {
+    item.setAttribute('src', item.getAttribute('data-src'))
+  })
 }, false)
