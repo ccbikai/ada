410d046248593a24b2298ec90aa762901aed0a1e
ccbikai(👎🏻🍜) <ccbikai@qq.com>
2018-05-09 17:12:23 +0800

    完善字体测试

--- docs/src/components/Static.jsx
+++ docs/src/components/Static.jsx
@@ -22,6 +22,15 @@ export default function Static () {
     <span className='iconfont icon-iPad' />
     <span className='iconfont icon-AppleWatchSeries' />
     <hr />
+    <h3>字体 测试</h3>
+    <div className='font'>
+            A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+      <br />
+            a b c d e f g h i j k l m n o p q r s t u v w x y z
+      <br />
+            1 2 3 4 5 6 7 8 9 0
+    </div>
+    <hr />
     <h3>其他页面</h3>
     <a className='link' href='./index.html'>Index</a>
     <a className='link' href='./test.html'>Test</a>
